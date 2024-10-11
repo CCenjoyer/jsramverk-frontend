5182d637c92b244ad03312bcf8c7e8d09d3f7a42
CarlOscarMarcus <oscar@wysarna.se>
2024-10-11 13:42:52 +0200

    Process apikey in .env

--- test/Componenttest.test.js
+++
@@ -1,9 +0,0 @@
-var assert = require('assert');
-
-describe('Math Test', function () {
-    describe('#Mathtest', function () {
-        it('Is 1+1 = 2?', function () {
-            assert.equal(1 + 1, 2);
-        });
-    });
-});
