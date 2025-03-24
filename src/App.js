ddced8a8dfd95f57d4c9c6c30cf10d0b25d3cda7
josf23 <josf23@student.bth.se>
2025-03-25 00:49:39 +0100

    removed ReactStrict

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+import React from "react";
 import AppRouter from "./Routes/AppRouter";
 
 function App() {
-  return (
-    <div className="App">
-        <AppRouter />
-        <div className="toast">
-          <div className="toast-body"></div>
+    return (
+        <div className="App">
+            <AppRouter />
+            <div className="toast">
+                <div className="toast-body"></div>
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default App;
