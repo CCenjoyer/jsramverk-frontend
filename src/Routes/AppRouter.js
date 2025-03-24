857de32fc96b87673e23c7b30f9305599242e081
josf23 <josf23@student.bth.se>
2025-03-25 00:50:26 +0100

    smaller changes for login feature

--- src/Routes/AppRouter.js
+++ src/Routes/AppRouter.js
@@ -2,48 +2,49 @@
  * React Components
  */
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 /**
  * Common Includes
  */
-import Menu from "../Components/Common/Menu";
 import Footer from "../Components/Common/Footer";
+import Menu from "../Components/Common/Menu";
 
 /**
  * Components Pages
  */
 import Index from "../Components/Index";
-import Update from "../Components/Page/Update";
 import About from "../Components/Page/About";
 import Contact from "../Components/Page/Contact";
 import Create from "../Components/Page/Create";
+import Document from "../Components/Page/Document";
 import Login from "../Components/Page/Login";
 
 function AppRouter() {
-  const basename = window.location.hostname.includes('student.bth.se') ? '/~josf23/editor' : '/';
+    const basename = window.location.hostname.includes("student.bth.se")
+        ? "/~josf23/editor"
+        : "/";
 
-  return (
-    <Router basename={basename}>
-      <header>
-        <Menu />
-      </header>
-      <main>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/update/:id" element={<Update />} />
-          {/* <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} /> */}
-          <Route path="/create" element={<Create />} />
+    return (
+        <Router basename={basename}>
+            <header>
+                <Menu />
+            </header>
+            <main>
+                <Routes>
+                    <Route path="/" element={<Index />} />
+                    <Route path="/document/:id" element={<Document />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/create" element={<Create />} />
+                    <Route path="/login" element={<Login />} />
 
-          <Route path="/login" element={<Login />} />
-          
-          <Route path="*" element={<h1>Page Not Found</h1>} />
-        </Routes>
-      </main>
-      <Footer />
-    </Router>
-  );
+                    <Route path="*" element={<h1>Page Not Found</h1>} />
+                </Routes>
+            </main>
+            <Footer />
+        </Router>
+    );
 }
 
 export default AppRouter;
